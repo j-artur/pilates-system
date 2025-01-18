0e04a1db91c029b7328ebf907b546b561a21000f
j-artur <jartur.dev@gmail.com>
2025-01-18 13:57:05 -0300

    init spring server

---
+++ server/src/main/java/dev/jartur/pilates_system/PilatesSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.jartur.pilates_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilatesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilatesSystemApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/dev/jartur/pilates_system/PilatesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jartur.pilates_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
